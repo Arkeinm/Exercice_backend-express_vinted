b0ede6df1e9a14566b664cfdb303437fb719af3a
Guillaume PATTI <guillaume.patti@gmail.com>
2024-04-02 08:16:58 +0200

    add package lock-json

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-mongoose.connect(process.env.MONGONDB_URI);
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGONDB_CLOUD);
 
 const Login = require("./routes/user");
 app.use(Login);
